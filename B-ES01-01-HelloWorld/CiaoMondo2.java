8eb70b112bac635cf248498d694f8e9cdb7e7364
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-09-07 17:27:56 +0000

    java

--- B-ES01-01-HelloWorld/CiaoMondo2.java
+++ B-ES01-01-HelloWorld/CiaoMondo2.java
@@ -5,17 +5,15 @@ class CiaoMondo2 {
     public static void main(String[] args) {
         String hello = "Ciao ";
         String world = "mondo\n";
+        
         System.out.print(hello.toUpperCase()); 
         for(int i=0; i<world.length(); i++) {
             System.out.print(world.charAt(i));
         }
-        System.out.print(hello.toUpperCase()); 
-        System.out.println("---"); 
-        for(int i=world.length(); i>0; i++) {
+        for(int i=world.length()-2; i>=0; i--) {
             System.out.print(world.charAt(i));
         }
-        System.out.println("---"); 
-        System.out.println("---"); 
+        System.out.print(" " + hello. + "\n"); 
         System.out.println("---"); 
     }
 }
