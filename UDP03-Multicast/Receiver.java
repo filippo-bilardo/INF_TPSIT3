118f3648ee9da06e6b8f47fb5982195936d8307d
Filippo <filippo.bilardo@tiscali.it>
2023-11-22 21:09:09 +0000

    update

--- UDP03-Multicast/Receiver.java
+++ UDP03-Multicast/Receiver.java
@@ -8,7 +8,9 @@
  */
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 
 public class Receiver {
 
@@ -19,27 +21,32 @@ public class Receiver {
     public static void main(String[] args) {
         try 
         {
-            //Creo il MulticastSocket per lo scambio dei dati con il client
+            // Creo il MulticastSocket per lo scambio dei dati con il client
             MulticastSocket socket = new MulticastSocket(MULTICAST_PORT);
+            // Ottengo l'indirizzo IP del gruppo di multicast
             InetAddress group = InetAddress.getByName(MULTICAST_IP);
-            socket.setInterface(InetAddress.getByName(RECEIVER_IP));
-            socket.joinGroup(group);
+            // Ottengo l'interfaccia di rete associata all'indirizzo IP del receiver
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(InetAddress.getByName(RECEIVER_IP));
+            // Ottengo l'indirizzo socket di multicast del gruppo
+            InetSocketAddress groupAddress = new InetSocketAddress(group, MULTICAST_PORT);
+            // Mi unisco al gruppo di multicast utilizzando l'interfaccia di rete
+            socket.joinGroup(groupAddress, networkInterface);
 
-            //Mi preparo alla ricezione dei dati
+            // Mi preparo alla ricezione dei dati
             byte[] buffer = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             System.out.println("Receiver in ascolto. Ctrl+C per terminare.");
             
-            //Ricevo il DatagramPacket dal client e lo stampo a schermo
+            // Ricevo il DatagramPacket dal client e lo stampo a schermo
             String message = "";
-            while (message != "bye") {
+            while (!message.equals("bye")) {
                 socket.receive(packet);
                 message = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Messaggio ricevuto: " + message);
             }
 
-            //Fine della comunicazione
-            socket.leaveGroup(group);
+            // Fine della comunicazione
+            socket.leaveGroup(groupAddress, networkInterface);
             socket.close();
             
         } catch (Exception e) {
